20547d5a805bb7d68ba38c10a2eb479de0f1e0da
Радмила <radmila.zaripova.d@gmail.com>
2023-04-07 14:19:43 +0500

    Попытка номер 3

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
         for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
