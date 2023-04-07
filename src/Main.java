02abd367edfde15609fe2bf69be8165f659846c1
Радмила <radmila.zaripova.d@gmail.com>
2023-04-07 14:22:23 +0500

    Попытка номер 5

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
 
 
-            // for you, but you can always add more by pressing Ctrl+F8.
+
             System.out.println("i = " + i);
         }
     }
